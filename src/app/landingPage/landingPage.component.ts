1576cf51736eda452d94cbba52fa6b59755a5894
sebastian_iwasieczko@web.de <you@example.com>
2025-09-20 18:12:03 +0200

    first changes

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landingPage.component.html',
+  styleUrl: './landingPage.component.scss',
+})
+export class LandingPageComponent {}
