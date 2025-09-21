0f65da8748cebca6095ea918776fb20eeaacbb2c
sebastian_iwasieczko@web.de <you@example.com>
2025-09-21 18:44:42 +0200

    generate navbar

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './landingPage.component.html',
   styleUrl: './landingPage.component.scss',
 })
